3fae69652b8eb0fa54d9e93423c0eb74644d5dc2
Yash Tiwari <118112664+yashtiwari615@users.noreply.github.com>
2024-03-31 19:41:48 +0530

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { readFileSync } from "fs";
+import path from "path";
+import { gql } from "graphql-tag";
+import { resolvers } from "./resolvers"
+import { SpotifyAPI } from "./datasources/spotify-api";
+
+const typeDefs = gql(
+  readFileSync(path.resolve(__dirname, "./schema.graphql"), {
+    encoding: "utf-8",
+  })
+);
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  const { url } = await startStandaloneServer(server, {
+    context: async () => {
+      const { cache } = server;
+      return {
+        dataSources: {
+          spotifyAPI: new SpotifyAPI({ cache }),
+        },
+      };
+    },
+  });
+  console.log(`
+    🚀  Server is running
+    📭  Query at ${url}
+  `);
+}
+
+startApolloServer();
